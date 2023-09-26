404cf1a2000ecb00f93ea2f35a97cba001db900c
kredsya <do901328@gmail.com>
2023-09-26 13:26:00 +0900

    Fix: segmentation fault

--- drop-packet.cpp
+++ drop-packet.cpp
@@ -1,4 +1,4 @@
-// #define DEBUG
+#define DEBUG
 
 #include "headers.h"
 #include "log.h"
@@ -11,6 +11,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pcap.h>
+
+struct _tcpPacket final {
+	struct _eth *eth;
+	struct _ip  *ip;
+	struct _tcp *tcp;
+};
 
 void usage() {
 	printf("usage: sudo ./drop-packet <interface>\n");
@@ -37,22 +43,34 @@ int main(int argc, char* argv[]) {
 	}
 
 	int cnt = 0;
+	_tcpPacket *tcpPacket = new _tcpPacket;
 	while(++cnt) {
 		struct pcap_pkthdr* header;
 		const uint8_t* packet;
 		int res = pcap_next_ex(pcap, &header, &packet);
 		if(res == 0) continue;
 
+		tcpPacket->eth = (struct _eth*)(packet);
+		tcpPacket->ip  = (struct _ip* )(packet + ETH_SIZE);
+		tcpPacket->tcp = (struct _tcp*)(packet + ETH_SIZE + IP_SIZE);
+		PRINT(tcpPacket->eth->type);
+		PRINT(tcpPacket->ip->proto);
+		printf("=============\n");
+		continue;
+
 		struct _eth *eth = (struct _eth*)(packet);
 		if(ntohs(eth->type) != ETH_TYPE_IPV4) continue;
 
 		struct _ip *ip = (struct _ip*)(packet + ETH_SIZE);
-		if(ip->proto != IP_PROTO_TCP && ip->proto != IP_PROTO_UDP) continue;
+		if(ip->proto != IP_PROTO_TCP) continue;
 
 		struct _tcp *tcp = (struct _tcp*)(packet + ETH_SIZE + IP_SIZE);
+		
 
 		printf("========%d========\n", cnt);
 	}
+
+	delete tcpPacket;
 	
 	return 0;
 }
