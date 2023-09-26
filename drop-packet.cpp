2a65ff2169578d32ac0af2630671358fe658da65
kredsya <do901328@gmail.com>
2023-09-27 00:33:58 +0900

    Add: print all of tcp log func / Add: some of packet construct

--- drop-packet.cpp
+++ drop-packet.cpp
@@ -4,8 +4,10 @@
 #include "log.h"
 
 // C++
+#include <fstream>
 #include <vector>
 #include <string>
+#include <utility>
 
 // C
 #include <stdio.h>
@@ -13,9 +15,9 @@
 #include <pcap.h>
 
 struct _tcpPacket final {
-	struct _eth *eth;
-	struct _ip  *ip;
-	struct _tcp *tcp;
+	struct _eth eth;
+	struct _ip  ip;
+	struct _tcp tcp;
 };
 
 void usage() {
@@ -25,6 +27,23 @@ void usage() {
 
 uint16_t ntohs(uint16_t n) {
 	return (n>>8) | (n<<8);
+}
+
+uint32_t ntohl(uint32_t n) {
+	return (n<<24) | (n<<8 & 0xFF0000) | (n>>8 & 0xFF00) | (n>>24 & 0xFF);
+}
+
+uint8_t hex2int(char n) {
+	if('0' <= n && n <= '9') return n - '0';
+	else return n - 'a' + 10;
+}
+
+uint8_t* str2arrayMac(std::string mac_str) {
+	uint8_t *ret = (uint8_t*)malloc(sizeof(uint8_t) * 6);
+	for(int i=0; i<6; i++) {
+		ret[i] = hex2int(mac_str[i*3])*16 + hex2int(mac_str[i*3+1]);
+	}
+	return ret;
 }
 
 int main(int argc, char* argv[]) {
@@ -42,31 +61,48 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
+	std::ifstream iface("/sys/class/net/" + std::string(handle) + "/address");
+	std::string mac_temp((std::istreambuf_iterator<char>(iface)), std::istreambuf_iterator<char>());
+	std::string mac_str(mac_temp.begin(), mac_temp.end()-1);
+#ifdef DEBUG
+	printf("%s\n", mac_str.c_str());
+#endif
+	uint8_t *my_mac = str2arrayMac(mac_str);
+
 	int cnt = 0;
 	_tcpPacket *tcpPacket = new _tcpPacket;
+	_tcpPacket *fwd = new _tcpPacket;
+	_tcpPacket *bwd = new _tcpPacket;
 	while(++cnt) {
 		struct pcap_pkthdr* header;
 		const uint8_t* packet;
 		int res = pcap_next_ex(pcap, &header, &packet);
 		if(res == 0) continue;
 
-		tcpPacket->eth = (struct _eth*)(packet);
-		tcpPacket->ip  = (struct _ip* )(packet + ETH_SIZE);
-		tcpPacket->tcp = (struct _tcp*)(packet + ETH_SIZE + IP_SIZE);
-		PRINT(tcpPacket->eth->type);
-		PRINT(tcpPacket->ip->proto);
-		printf("=============\n");
-		continue;
+		tcpPacket->eth = *((struct _eth*)(packet));
+		tcpPacket->ip  = *((struct _ip* )(packet + ETH_SIZE));
+		tcpPacket->tcp = *((struct _tcp*)(packet + ETH_SIZE + IP_SIZE));
 
-		struct _eth *eth = (struct _eth*)(packet);
-		if(ntohs(eth->type) != ETH_TYPE_IPV4) continue;
+		if(ntohs(tcpPacket->eth.type) != ETH_TYPE_IPV4) continue;
+		if(tcpPacket->ip.proto != IP_PROTO_TCP) continue;
+		if(tcpPacket->tcp.flags2 != TCP_FLAGS_ACK) continue;
 
-		struct _ip *ip = (struct _ip*)(packet + ETH_SIZE);
-		if(ip->proto != IP_PROTO_TCP) continue;
+		fwd->eth = bwd->eth = tcpPacket->eth;
+		fwd->ip  = bwd->ip  = tcpPacket->ip;
+		fwd->tcp = bwd->tcp = tcpPacket->tcp;
 
-		struct _tcp *tcp = (struct _tcp*)(packet + ETH_SIZE + IP_SIZE);
-		
+		for(int i=0; i<6; i++)
+			fwd->eth.src[i] = my_mac[i];
+		for(int i=0; i<6; i++)
+			bwd->eth.dst[i] = bwd->eth.src[i];
+		for(int i=0; i<6; i++)
+			bwd->eth.src[i] = my_mac[i];
 
+		fwd->ip.len = bwd->ip.len = IP_SIZE + fwd->tcp.hdr_len;
+		std::swap(bwd->ip.src, bwd->ip.dst);
+
+		PRINT_TCP(fwd);
+		PRINT_TCP(bwd);
 		printf("========%d========\n", cnt);
 	}
 
--- log.h
+++ log.h
@@ -6,9 +6,20 @@
 #ifdef DEBUG
 #define LOG(MSG) printf("[LOG] %s/%s(%d): %s\n", __FILE__, __FUNCTION__, __LINE__, MSG);
 #define PRINT(x) printf("[VAR] %s/%s(%d): %s = %x\n", __FILE__, __FUNCTION__, __LINE__, #x, x);
+#define PRINT_TCP(p) printf("[TCP] %s/%s(%d): %s \n %02x:%02x:%02x:%02x:%02x:%02x | %02x:%02x:%02x:%02x:%02x:%02x \n %d.%d.%d.%d | %d.%d.%d.%d \n %d -> %d \n %d vs %d \n %d \n -------- \n",\
+		__FILE__, __FUNCTION__, __LINE__, #p,\
+		p->eth.src[0], p->eth.src[1], p->eth.src[2], p->eth.src[3], p->eth.src[4], p->eth.src[5],\
+		p->eth.dst[0], p->eth.dst[1], p->eth.dst[2], p->eth.dst[3], p->eth.dst[4], p->eth.dst[5],\
+		p->ip.srcmask[0], p->ip.srcmask[1], p->ip.srcmask[2], p->ip.srcmask[3],\
+		p->ip.srcmask[0], p->ip.srcmask[1], p->ip.dstmask[2], p->ip.dstmask[3],\
+		ntohs(p->tcp.srcport), ntohs(p->tcp.dstport),\
+		ntohl(p->tcp.seq_raw), ntohl(p->tcp.ack_raw),\
+		ntohs(p->tcp.checksum)\
+		);
 #else
 #define LOG(MSG) ((void)0);
 #define PRINT(x) ((void)0);
+#define PRINT_TCP(p) ((void(0));
 #endif	// DEBUG
 
 #endif	// LOG_H_
