6c305203ac0db54ae3c8aef9325ce7272f77a238
kredsya <do901328@gmail.com>
2023-10-16 07:07:43 +0900

    Refactoring: for new headers.h and something more change

--- main.cpp
+++ main.cpp
@@ -15,6 +15,8 @@
 #include <stdlib.h>
 #include <pcap.h>
 
+char errbuf[PCAP_ERRBUF_SIZE];
+
 #pragma pack(push, 1)
 struct _tcpPacket final {
 	struct _eth eth;
@@ -28,17 +30,36 @@ void usage() {
 	printf("example: sudo ./block-packet enp0s3 eth0\n");
 }
 
-uint8_t hex2int(char n) {
-	if('0' <= n && n <= '9') return n - '0';
-	else return n - 'a' + 10;
+pcap_t* open_pcap(char* interface) {
+	pcap_t* pcap = pcap_open_live(interface, BUFSIZ, 1, 1, errbuf);
+	if(pcap == NULL) {
+		fprintf(stderr, "pcap_open_live(%s) return null - %s\n", interface, errbuf);
+		return NULL;
+	}
+	return pcap;
 }
 
-uint8_t* str2arrayMac(std::string mac_str) {
-	uint8_t *ret = (uint8_t*)malloc(sizeof(uint8_t) * 6);
+uint8_t hex2int(char n) {
+	return ('0' <= n && n <= '9') ? n - '0' : n - 'a' + 10;
+}
+
+uint8_t* resolve_mac(char* interface) {
+	std::ifstream iface("/sys/class/net/" + std::string(interface) + "/address");
+	std::string mac((std::istreambuf_iterator<char>(iface)), std::istreambuf_iterator<char>());
+	mac.pop_back();
+	uint8_t *ret = (uint8_t*)malloc(sizeof(uint8_t) * 8);
 	for(int i=0; i<6; i++) {
-		ret[i] = hex2int(mac_str[i*3])*16 + hex2int(mac_str[i*3+1]);
+		ret[i] = hex2int(mac[i*3])*16 + hex2int(mac[i*3+1]);
 	}
 	return ret;
+}
+
+bool custom_filter(_tcpPacket *pkt) {
+	if(pkt->eth.type() != _eth::ipv4) return true;
+	if(pkt->ip.proto() != _ip::tcp) return true;
+	if(pkt->tcp.flags2() != _tcp::flags_ack) return true;
+
+	return false;
 }
 
 int main(int argc, char* argv[]) {
@@ -47,34 +68,22 @@ int main(int argc, char* argv[]) {
 		usage();
 		return -1;
 	}
-	char* mirror_handle = argv[1];
-	char* send_handle = argv[2];
+	char* mirror_interface = argv[1];
+	char* send_interface = argv[2];
 
-	char errbuf[PCAP_ERRBUF_SIZE];
-	pcap_t* mirror_pcap = pcap_open_live(mirror_handle, BUFSIZ, 1, 1, errbuf);
-	if(mirror_pcap == NULL) {
-		fprintf(stderr, "pcap_open_live(%s) return null - %s\n", mirror_handle, errbuf);
-		return -1;
-	}
-	pcap_t* send_pcap = pcap_open_live(send_handle, BUFSIZ, 1, 1, errbuf);
-	if(send_pcap == NULL) {
-		fprintf(stderr, "pcap_open_live(%s) return null - %s\n", send_handle, errbuf);
+	pcap_t* mirror_pcap = open_pcap(mirror_interface);
+	pcap_t* send_pcap = open_pcap(send_interface);
+	if(mirror_pcap == NULL || send_pcap == NULL) {
 		return -1;
 	}
 
-	std::ifstream iface("/sys/class/net/" + std::string(send_handle) + "/address");
-	std::string mac_temp((std::istreambuf_iterator<char>(iface)), std::istreambuf_iterator<char>());
-	std::string mac_str(mac_temp.begin(), mac_temp.end()-1);
-#ifdef DEBUG
-	printf("%s\n", mac_str.c_str());
-#endif
-	uint8_t *my_mac = str2arrayMac(mac_str);
+	uint8_t *my_mac = resolve_mac(send_interface);
 
-	int cnt = 0;
+	int pkt_cnt = 0;
 	_tcpPacket *tcpPacket = new _tcpPacket;
 	_tcpPacket *fwd = new _tcpPacket;
 	_tcpPacket *bwd = new _tcpPacket;
-	while(++cnt) {
+	while(++pkt_cnt) {
 		struct pcap_pkthdr* header;
 		const uint8_t* packet;
 		int res = pcap_next_ex(mirror_pcap, &header, &packet);
@@ -84,55 +93,48 @@ int main(int argc, char* argv[]) {
 		tcpPacket->ip  = *((struct _ip* )(packet + ETH_SIZE));
 		tcpPacket->tcp = *((struct _tcp*)(packet + ETH_SIZE + tcpPacket->ip.ip_size()));
 
-		if(ntohs(tcpPacket->eth.type) != tcpPacket->eth.IPv4) continue;
-		if(tcpPacket->ip.proto != IP_PROTO_TCP) continue;
-		if(tcpPacket->tcp.flags2 != TCP_FLAGS_ACK) continue;
+		// modify custom_filter() function
+		// it must return true, when a packet is recieved what you don't need
+		if(custom_filter(tcpPacket)) continue;
 
 		fwd->eth = bwd->eth = tcpPacket->eth;
 		fwd->ip  = bwd->ip  = tcpPacket->ip;
 		fwd->tcp = bwd->tcp = tcpPacket->tcp;
 
-
-		//PRINT_TCP(fwd);
-		//PRINT_TCP(bwd);
-
 		for(int i=0; i<6; i++)
-			fwd->eth.src[i] = bwd->eth.src[i] = my_mac[i];
+			fwd->eth._src[i] = bwd->eth._src[i] = my_mac[i];
 		for(int i=0; i<6; i++)
-			bwd->eth.dst[i] = tcpPacket->eth.src[i];
+			bwd->eth._dst[i] = tcpPacket->eth._src[i];
 
-		uint16_t ip_size = tcpPacket->ip.hdr_len * 4;
-		uint16_t tcp_size = tcpPacket->tcp.hdr_len * 4;
-		fwd->ip.len = bwd->ip.len = ntohs(ip_size + tcp_size);
-		std::swap(bwd->ip.src, bwd->ip.dst);
-		bwd->ip.ttl = 128;
+		fwd->ip._len = bwd->ip._len = ntohs(tcpPacket->ip.ip_size() + tcpPacket->tcp.tcp_size());
+		std::swap(bwd->ip._src, bwd->ip._dst);
+		bwd->ip._ttl = 128;
 
-		std::swap(bwd->tcp.srcport, bwd->tcp.dstport);
-		uint16_t tcp_data_size = ntohs(tcpPacket->ip.len) - ip_size - tcp_size;
-		fwd->tcp.seq_raw = ntohl(tcpPacket->tcp.seq_raw + tcp_data_size);
-		bwd->tcp.seq_raw = tcpPacket->tcp.seq_raw;
-		fwd->tcp.flags2 = TCP_FLAGS_RSTACK;
-		bwd->tcp.flags2 = TCP_FLAGS_RSTACK;
+		std::swap(bwd->tcp._srcport, bwd->tcp._dstport);
+		fwd->tcp._seq_raw = ntohl(tcpPacket->tcp.seq_raw() + _tcp::len(&(tcpPacket->ip), &(tcpPacket->tcp)));
+		bwd->tcp._seq_raw = tcpPacket->tcp._seq_raw;
+		fwd->tcp._flags2 = _tcp::flags_rstack;
+		bwd->tcp._flags2 = _tcp::flags_rstack;
 
 
-		fwd->ip.checksum = _ip::calcIpChecksum(&(fwd->ip));
-		bwd->ip.checksum = _ip::calcIpChecksum(&(bwd->ip));
+		fwd->ip._checksum = _ip::calcIpChecksum(&(fwd->ip));
+		bwd->ip._checksum = _ip::calcIpChecksum(&(bwd->ip));
 
-		fwd->tcp.checksum = _tcp::calcTcpChecksum(&(fwd->ip), &(fwd->tcp));
-		bwd->tcp.checksum = _tcp::calcTcpChecksum(&(bwd->ip), &(bwd->tcp));
+		fwd->tcp._checksum = _tcp::calcTcpChecksum(&(fwd->ip), &(fwd->tcp));
+		bwd->tcp._checksum = _tcp::calcTcpChecksum(&(bwd->ip), &(bwd->tcp));
 
 		res = pcap_sendpacket(send_pcap, reinterpret_cast<const u_char*>(fwd), sizeof(_tcpPacket));
 		GTRACE("send forward packet");
 		if(res != 0) {
-			fprintf(stderr, "pcap_sendpacket(%s) of fwd return null - %s\n", send_handle, errbuf);
+			fprintf(stderr, "pcap_sendpacket(%s) of fwd return null - %s\n", send_interface, errbuf);
 		}
 		res = pcap_sendpacket(send_pcap, reinterpret_cast<const u_char*>(bwd), sizeof(_tcpPacket));
 		GTRACE("send backward packet");
 		if(res != 0) {
-			fprintf(stderr, "pcap_sendpacket(%s) of bwd return null - %s\n", send_handle, errbuf);
+			fprintf(stderr, "pcap_sendpacket(%s) of bwd return null - %s\n", send_interface, errbuf);
 		}
 
-		printf("========%d========\n", cnt);
+		printf("========%d========\n", pkt_cnt);
 	}
 
 	delete tcpPacket;
