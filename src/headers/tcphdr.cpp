4c049f85a27a5812b94d0b3e92a464300b9accd8
Kredsya <do901328@gmail.com>
2023-11-08 10:58:07 +0900

    Modify: return tcp to legacy calctcp

--- src/headers/tcphdr.cpp
+++ src/headers/tcphdr.cpp
@@ -11,15 +11,14 @@ uint16_t TcpHdr::payloadLen(IpHdr *iphdr, TcpHdr *tcphdr) {
 */
 uint16_t TcpHdr::calcTcpChecksum(IpHdr *iphdr, TcpHdr *tcphdr) {
     uint32_t ret = 0;
-    uint16_t *pword = reinterpret_cast<uint16_t*>(tcphdr);
-    tcphdr->checksum_ = 0;
+    tcp->_checksum = 0;
     
-    ret += (ntohs(iphdr->src() >> 16)) + (ntohs(iphdr->src() & 0xFFFF));
-    ret += (ntohs(iphdr->dst() >> 16)) + (ntohs(iphdr->dst() & 0xFFFF));
-    ret += iphdr->proto();
-    ret += tcphdr->tcpHdrSize();
-    for(int i=0; i<tcphdr->tcpHdrSize(); i+=2) {
-        ret += ntohs(*(pword + i));
+    ret += (ntohs(ip->src() >> 16)) + (ntohs(ip->src() & 0xFFFF));
+    ret += (ntohs(ip->dst() >> 16)) + (ntohs(ip->dst() & 0xFFFF));
+    ret += ip->proto();
+    ret += tcp->tcp_size();
+    for(int i=0; i<tcp->tcp_size(); i+=2) {
+        ret += (*((uint8_t*)tcp + i) << 8) + (*((uint8_t*)tcp + i + 1));
     }
     ret += ret >> 16;
     return ntohs(~(uint16_t)ret);
