09674368c35dca81d6a4f5438e162aff48704246
Kredsya <do901328@gmail.com>
2023-11-11 03:25:18 +0900

    Modify: collapse all hdr to RxPacket and add parse() method

--- src/packet.h
+++ src/packet.h
@@ -2,72 +2,83 @@
 #define PACKET_H_
 
 #include "headers/ethhdr.h"
+
 #include "headers/iphdr.h"
+#include "headers/grehdr.h"
+
 #include "headers/tcphdr.h"
 #include "headers/udphdr.h"
-#include "headers/grehdr.h"
 #include "headers/ppphdr.h"
-#include "headers/lcphdr.h"
+
 #include "headers/openvpntcphdr.h"
+#include "headers/tlshdr.h"
+
+#include "headers/lcphdr.h"
 
 #pragma pack(push, 1)
 struct RxPacket {
     struct EthHdr *ethhdr{nullptr};
     
     struct IpHdr *iphdr{nullptr};
+    struct GreHdr *grehdr{nullptr};
     
     struct TcpHdr *tcphdr{nullptr};
     struct UdpHdr *udphdr{nullptr};
+    struct PppHdr *ppphdr{nullptr};
     
-    struct GreHdr *grehdr{nullptr};
+    struct OpenVpnTcpHdr *openvpntcphdr{nullptr};
+    struct TlsHdr *tlshdr{nullptr};
+    
+    struct LcpHdr *lcphdr{nullptr};
     
     void clear() {
         ethhdr = nullptr;
+        
         iphdr = nullptr;
+        grehdr = nullptr;
+        
         tcphdr = nullptr;
         udphdr = nullptr;
-        grehdr = nullptr;
+        ppphdr = nullptr;
+        
+        openvpntcphdr = nullptr;
+        tlshdr = nullptr;
+        
+        lcphdr = nullptr;
     }
     
-    virtual void parse(const uint8_t *pkt) {
-        this->ethhdr = (struct EthHdr*)(pkt);
-        this->iphdr = (struct IpHdr*)(pkt + ETH_SIZE);
-	    this->tcphdr = (struct TcpHdr*)(pkt + ETH_SIZE + this->iphdr->ipHdrSize());
-    }
-};
-
-struct RxOpenVpnTcpPacket : RxPacket {
-    struct OpenVpnTcpHdr *openvpntcphdr{nullptr};
-    
-    RxOpenVpnTcpPacket() : RxPacket() {
-        openvpntcphdr = new OpenVpnTcpHdr;
-    }
-    
-    void parse(const uint8_t *pkt) override {
-        this->ethhdr = (struct EthHdr*)(pkt);
-        this->iphdr = (struct IpHdr*)(pkt + ETH_SIZE);
-	    this->tcphdr = (struct TcpHdr*)(pkt + ETH_SIZE + this->iphdr->ipHdrSize());
-        this->openvpntcphdr = (struct OpenVpnTcpHdr*)(pkt + ETH_SIZE + this->iphdr->ipHdrSize() + this->tcphdr->tcpHdrSize());
-    }
-};
-
-struct RxPppPacket : RxPacket {
-    struct PppHdr *ppphdr{nullptr};
-    struct LcpHdr *lcphdr{nullptr};
-    
-    RxPppPacket() : RxPacket() {
-        ppphdr = new PppHdr;
-        lcphdr = new LcpHdr;
-    }
-    
-    void parse(const uint8_t *pkt) override {
-        // @todo
+    void parse(const uint8_t *pkt) {
+        ethhdr = (struct EthHdr*)(pkt);
+        switch(ethhdr->type()) {
+        case EthHdr::ipv4:
+            iphdr = (struct IpHdr*)(pkt + ETH_SIZE);
+            switch(iphdr->proto()) {
+            case IpHdr::tcp:
+                tcphdr = (struct TcpHdr*)(pkt + ETH_SIZE + iphdr->ipHdrSize());
+                if(tcphdr->dstport() == TcpHdr::tls) {
+                    tlshdr->parse(pkt + ETH_SIZE + iphdr->ipHdrSize() + tcphdr->tcpHdrSize());
+                }
+                openvpntcphdr = (struct OpenVpnTcpHdr*)(pkt + ETH_SIZE + iphdr->ipHdrSize() + tcphdr->tcpHdrSize());
+                break;
+            case IpHdr::udp:
+                udphdr = (struct UdpHdr*)(pkt + ETH_SIZE + iphdr->ipHdrSize());
+                break;
+            case IpHdr::gre:
+                grehdr = (struct GreHdr*)(pkt + ETH_SIZE + iphdr->ipHdrSize());
+                if(grehdr->proto() == GreHdr::ppp) {
+                    ppphdr = (struct PppHdr*)(pkt + ETH_SIZE + iphdr->ipHdrSize() + grehdr->greHdrSize());
+                    if(ppphdr->protocol() == PppHdr::lcp) {
+                        lcphdr = (struct LcpHdr*)(pkt + ETH_SIZE + iphdr->ipHdrSize() + grehdr->greHdrSize() + PPP_SIZE());
+                    }
+                }   // grehdr
+                break;
+            }   // iphdr
+            break;
+        }
     }
 };
 
 struct TxPacket {
-    // raw socket does not need ETH
-    // struct EthHdr eth;
     struct IpHdr iphdr;
     struct TcpHdr tcphdr;
 };
