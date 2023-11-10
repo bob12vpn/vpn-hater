b1dfbbf401630844fdc9fd0086d54b3f4f60413f
Kredsya <do901328@gmail.com>
2023-11-11 03:22:04 +0900

    Add: struct TlsHdr

---
+++ src/headers/tlshdr.cpp
@@ -0,0 +1,59 @@
+#include "tlshdr.h"
+
+uint16_t read16b(const uint8_t *c) {
+	return (uint16_t)(c[0] << 8 | c[1]);
+}
+uint32_t read24b(const uint8_t *c) {
+	return (uint32_t)(c[1] << 16 | c[2] << 8 | c[3]);
+}
+
+void TlsHdr::parse(const uint8_t *pkt) {
+    int offset = 0;
+    
+    contentType_ = (uint8_t)pkt[offset];
+    offset += 3;
+    if(contentType() != TlsHdr::handshake) return;
+    
+    length_ = (uint16_t)(pkt[offset] << 8 | pkt[offset + 1]);
+    offset += 2;
+    
+    type_ = (uint8_t)pkt[offset];
+    offset += 1;
+    if(type() != TlsHdr::clientHello) return;
+    
+    handshakeLength_ = (uint32_t)(pkt[offset + 1] << 16 | pkt[offset + 2] << 8 | pkt[offset + 3]);
+    offset += 37;
+    
+    sessionIdLength_ = (uint8_t)pkt[offset];
+    offset += sessionIdLength();
+    
+    cipherSuitesLength_ = (uint16_t)(pkt[offset] << 8 | pkt[offset + 1]);
+    offset += cipherSuitesLength();
+    
+    compMethodsLength_ = (uint8_t)pkt[offset];
+    offset += compMethodsLength();
+    
+    extensionsLength_ = (uint16_t)(pkt[offset] << 8 | pkt[offset + 1]);
+    offset += 2;
+    
+    while(true) {
+        extensionType_ = (uint16_t)(pkt[offset] << 8 | pkt[offset + 1]);
+        offset += 2;
+        
+        len_ = (uint16_t)(pkt[offset] << 8 | pkt[offset + 1]);
+        offset += 2;
+        
+        if(extensionType_ == TlsHdr::serverName) {
+            offset += 3;
+            
+            serverNameLen_ = (uint16_t)(pkt[offset] << 8 | pkt[offset + 1]);
+            offset += 2;
+            
+            serverName_ = std::string(pkt + offset, pkt + offset + serverNameLen());
+            break;
+        }
+        else {
+            offset += len_;
+        }
+    }
+}
---
+++ src/headers/tlshdr.h
@@ -0,0 +1,58 @@
+#ifndef TLSHDR_H_
+#define TLSHDR_H_
+
+#include "hpch.h"
+
+
+#define	TLS_SIZE	5
+
+
+#pragma pack(push, 1)
+struct TlsHdr {
+	// record field
+	uint8_t contentType_;
+	uint16_t length_;
+	
+	// handshake field
+	uint8_t type_;
+	uint32_t handshakeLength_;
+	uint8_t sessionIdLength_;
+	uint16_t cipherSuitesLength_;
+	uint8_t compMethodsLength_;
+	uint16_t extensionsLength_;
+	
+	// extension field
+	uint16_t extensionType_;
+	uint16_t len_;
+	
+	// server name field
+	uint16_t serverNameLen_;
+	std::string serverName_;
+
+	uint8_t contentType() { return contentType_; }
+	uint16_t length() { return length_; }
+	
+	uint8_t type() { return type_; };
+	uint32_t handshakeLength() { return handshakeLength_; }
+	uint8_t sessionIdLength() { return sessionIdLength_; }
+	uint16_t cipherSuitesLength() { return cipherSuitesLength_; }
+	uint8_t compMethodsLength() { return compMethodsLength_; }
+	uint16_t extensionsLength() { return extensionsLength_; }
+	
+	uint16_t extensionType() { return extensionType_; }
+	uint16_t len() { return len_; }
+	
+	uint16_t serverNameLen() { return serverNameLen_; }
+	std::string serverName() { return serverName_; }
+	
+	void parse(const uint8_t*);
+	
+	enum : uint8_t {
+        handshake = 22,
+		clientHello = 1,
+		serverName = 0
+	};
+};
+#pragma pack(pop)
+
+#endif // TLSHDR_H_
