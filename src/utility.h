5d886930cda63db023473ceb94de567e053739a5
Kredsya <do901328@gmail.com>
2023-11-11 05:16:24 +0900

    Delete: unneeded parsing function

--- src/utility.h
+++ src/utility.h
@@ -12,7 +12,4 @@ uint8_t hex2int(char);
 
 bool loadSni(char*, std::unordered_set<std::string>&);
 
-void parsingPacket(RxPacket*, const uint8_t*);
-void parsingPacket(RxOpenVpnTcpPacket*, const uint8_t*);
-
 #endif // UTILITY_H_
