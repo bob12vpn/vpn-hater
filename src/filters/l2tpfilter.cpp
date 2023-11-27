230c8720fee15e81287f975bfe0119c42e8b3435
idkdyk <syousz@cau.ac.kr>
2023-11-28 01:18:38 +0900

    Added l2tpfilter.h, l2tpfilter.cpp

---
+++ src/filters/l2tpfilter.cpp
@@ -0,0 +1,53 @@
+#include "l2tpfilter.h"
+
+bool L2tpFilter::process(RxPacket *rxPacket) {
+    if(rxPacket->ethhdr != nullptr && rxPacket->ethhdr->type() != EthHdr::ipv4) return false;
+	if(rxPacket->iphdr != nullptr && rxPacket->iphdr->proto() != IpHdr::udp) return false;
+    if(rxPacket->iphdr != nullptr && (rxPacket->udphdr->dstport() == 1701 || rxPacket->udphdr->srcport() == 1701)) return false;
+    if(rxPacket->iphdr->id() == 0x4444) return false;
+
+
+    // copy packet
+    fwd->iphdr  = *(rxPacket->iphdr);
+    fwd->udphdr = *(rxPacket->udphdr);
+    fwd->l2tphdr = *(rxPacket->l2tphdr);
+
+
+    // modify ip header
+    fwd->iphdr.id_ = 0x4444;
+    fwd->iphdr.flags_ = 0x000;
+    fwd->iphdr.ttl_ = 128;
+
+    // modify udp header
+    
+    //modify l2tp header
+    fwd->l2tphdr.flags_ = 0x0002
+    //fwd->l2tphdr.tunnel_ =
+    //fwd->l2tphdr.session_ =    
+
+
+    //modify ppp header    
+    fwd->ppphdr.address_ = 0xFF;
+    fwd->ppphdr.control_ = 0x03;
+    fwd->ppphdr.protocol_ = htons(PppHdr::lcp);
+
+    //LCP header setup
+    fwd->lcphdr.code_ = 0x05;
+    fwd->lcphdr.identifier_ = 0x01;
+    
+    //setup lcpdhr length
+    fwd->lcphdr.length_ = htons(LCP_SIZE);
+    //setup udphdr length
+    fwd->udpHdr.length_ = htons(UDP_SIZE + L2TP_SIZE + PPP_SIZE + LCP_SIZE);
+    //setup iphdr length
+    uint16_t ipTotalLength = fwd->iphdr.ipHdrSize() + fwd->udphdr.udpHdrSize() + ntohs(fwd->udphdr.length_ - UDP_SIZE); //Need to add udpHdrSize 
+    fwd->iphdr.len_ = htons(ipTotalLength);
+
+    // calculate ip and tcp checksum
+    fwd->iphdr.checksum_ = IpHdr::calcIpChecksum(&(fwd->iphdr));
+    
+    // send packet
+    sendSocket.sendto(fwd);
+    
+    return true;
+}
