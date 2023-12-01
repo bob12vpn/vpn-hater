f744aa0c4f1d4f07aaca498d2835ee3b8780bd8b
Kredsya <do901328@gmail.com>
2023-12-01 16:53:25 +0900

    Fix: iphdr checksum

--- src/headers/iphdr.cpp
+++ src/headers/iphdr.cpp
@@ -2,12 +2,15 @@
 
 uint16_t IpHdr::calcIpChecksum(IpHdr *iphdr) {
     uint32_t ret = 0;
-    uint16_t *pword = reinterpret_cast<uint16_t *>(iphdr);
     iphdr->checksum_ = 0;
 
-    for (int i = 0; i < iphdr->ipHdrSize(); i += 2) {
-        ret += ntohs(*(pword + i));
+    uint16_t len = iphdr->ipHdrSize();
+    uint16_t *pword = reinterpret_cast<uint16_t *>(iphdr);
+    for (int i = 0; i < len / 2; i++) {
+        ret += ntohs(*pword);
+        pword++;
     }
+
     ret += ret >> 16;
     return ntohs(~(uint16_t)ret);
 }
